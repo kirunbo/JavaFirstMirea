1e1b613851d0defb822fb985225e57164b675fc8
kirunbo <kirunbo@gmail.com>
2024-11-19 17:47:57 +0300

    .

--- src/ru/mirea/task1/opt3/ArraySum.java
+++ src/ru/mirea/task1/opt3/ArraySum.java
@@ -1,5 +1,29 @@
 package ru.mirea.task1.opt3;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraySum {
+    public static void main(String[] args) {
+        //3
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[10];
+        int s = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+            s += array[i];
+        }
+        System.out.println(s);
+        System.out.println((double) s / array.length);
 
+        //5
+        for(String str : args){
+            System.out.println(str);
+        }
+
+        //6
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("1/%s ", i);
+        }
+
+    }
 }
